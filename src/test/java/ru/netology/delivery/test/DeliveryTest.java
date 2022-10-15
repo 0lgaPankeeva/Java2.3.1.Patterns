88eeb5d578f03aee6855c0be3196674db49d8491
Pankeeva Olga <0lga.pankeeva@mail.ru>
2022-10-15 22:13:32 +0400

    commit correct test

--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -47,7 +47,7 @@ public class DeliveryTest {
                 .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $(withText("Перепланировать")).click();
         $("[data-test-id=success-notification]").shouldBe(Condition.visible, Duration.ofSeconds(60));
-        $("[data-test-id='success-notification']")
+        $("[data-test-id=success-notification] .notification__content")
                 .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate));
     }
 }
